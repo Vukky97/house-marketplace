c0d42c657e3dba51f1e1ca28056ffde55ac6bc4a
Stephen White <37700241+Vukky97@users.noreply.github.com>
2022-02-21 14:55:22 +0100

    add explore page + fetch listings from firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Category from './pages/Category';
 import ForgotPassword from './pages/ForgotPassword';
 import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           {/* Private route with outlet (child element render) */}
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
