dc4c24884fcf16657f956929d20d19a6827d7d65
Stephen White <37700241+Vukky97@users.noreply.github.com>
2022-02-21 18:14:08 +0100

    add create listings page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Category from './pages/Category';
 import ForgotPassword from './pages/ForgotPassword';
+import CreateListing from './pages/CreateListing';
 import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -28,6 +29,7 @@ function App() {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
