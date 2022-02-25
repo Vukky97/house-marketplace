f5a0c18df849124112819faff875145eb924cfcf
Stephen White <37700241+Vukky97@users.noreply.github.com>
2022-02-25 11:38:30 +0100

    add load more listing and basic edit listing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
 import Contact from './pages/Contact';
+import EditListing from './pages/EditListing';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // minified version is also included
@@ -32,6 +33,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/edit-listing:listingId' element={<EditListing />} />
           <Route
             path='/category/:categoryName/:listingId'
             element={<Listing />}
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -18,6 +18,7 @@ import ListingItem from '../components/ListingItem';
 function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
   const params = useParams();
 
@@ -38,6 +39,9 @@ function Category() {
         // Execute query
         const querySnap = await getDocs(q);
 
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
         const listings = [];
 
         querySnap.forEach((doc) => {
@@ -56,6 +60,43 @@ function Category() {
     fetchListings();
   }, [params.categoryName]);
 
+  // Pagination / Load more
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, 'listings');
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where('type', '==', params.categoryName),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error('Could not fetch listings');
+    }
+  };
+
   return (
     <div className='category'>
       <header>
@@ -70,7 +111,7 @@ function Category() {
       ) : listings && listings.length > 0 ? (
         <>
           <main>
-            <div className='categoryListings'>
+            <ul className='categoryListings'>
               {listings.map((listing) => (
                 <ListingItem
                   listing={listing.data}
@@ -78,8 +119,16 @@ function Category() {
                   key={listing.id}
                 />
               ))}
-            </div>
+            </ul>
           </main>
+
+          <br />
+          <br />
+          {lastFetchedListing && (
+            <p className='loadMore' onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
         </>
       ) : (
         <p>No listints for {params.categoryName}</p>
